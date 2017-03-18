6ff6e08c56f4bc178ba351ae6fd46c765b0c853f
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-03-18 13:42:10 +0900

    プロジェクト ファイルを追加します。

---
+++ plecture/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="plecture.MvcApplication" Language="C#" %>
---
+++ plecture/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(plecture.Startup))]
+namespace plecture
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
